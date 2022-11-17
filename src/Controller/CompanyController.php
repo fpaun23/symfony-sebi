8f945a049fbacbf0664eab30cb2df2470d4c0f1e
Sebastian <gabrielsebijiga@gmail.com>
2022-11-17 14:48:51 +0200

    changes made

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -5,13 +5,21 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * new class for company controller
+ */
 class CompanyController extends AbstractController
 {
     public $page_title = 'Company Controller';
     public $name = 'Devnest';
+    
+    /**
+     * Loads the template for the company controller
+     *
+     * @return Response
+     */
     public function loadTemplate(): Response
     {
         return $this->render('company/index.html.twig', [
